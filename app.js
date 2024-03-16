b730d0ad4fa9976c32bd0484fff50191e89b55a9
hiteshSoftwareEngineer786 <chawlahitesh8907@gmail.com>
2024-03-16 15:19:57 +0530

    product detail

--- app.js
+++ app.js
@@ -87,85 +87,85 @@ app.get("/product/:id",async(req,res)=>{
     })
     // res.send("teting")
 })
-// app.get("*", (req, res) => {
-//   console.log("Requested path:", req.path);
-//   const filePath = path.join(__dirname, "/dist", "index.html");
-//   console.log(filePath, "filePath");
-//   fs.readFile(filePath, "utf8", async (err, data) => {
-//     if (err) {
-//       // Handle file not found or read error
-//       console.error(err);
-//       res.status(404).send("File not found");
-//     } else {
-//       if (req.path.startsWith("/product/")) {
-//         const productId = req.path.split("/")[2];
-//         console.log(productId);
-//         const resData = await fetch(
-//           `https://dummyjson.com/products/${productId}`
-//         );
-//         const res = await resData.json();
-//             console.log({data})
-//             data = data.replace(
-//                 /<meta property="og:image" content=".*?"/i,
-//                 `<meta property="og:image" content="${res?.thumbnail}"`
-//               );
-//               data = data.replace(
-//                 /<meta property="twitter:image" content=".*?"/i,
-//                 `<meta property="twitter:image" content="${res?.thumbnail}"`
-//               );
+app.get("*", (req, res) => {
+  console.log("Requested path:", req.path);
+  const filePath = path.join(__dirname, "/dist", "index.html");
+  console.log(filePath, "filePath");
+  fs.readFile(filePath, "utf8", async (err, data) => {
+    if (err) {
+      // Handle file not found or read error
+      console.error(err);
+      res.status(404).send("File not found");
+    } else {
+      if (req.path.startsWith("/product/")) {
+        const productId = req.path.split("/")[2];
+        console.log(productId);
+        const resData = await fetch(
+          `https://dummyjson.com/products/${productId}`
+        );
+        const res = await resData.json();
+            console.log({data})
+            data = data.replace(
+                /<meta property="og:image" content=".*?"/i,
+                `<meta property="og:image" content="${res?.thumbnail}"`
+              );
+              data = data.replace(
+                /<meta property="twitter:image" content=".*?"/i,
+                `<meta property="twitter:image" content="${res?.thumbnail}"`
+              );
               
-//               data = data.replace(
-//                 /<meta property="og:title" content=".*?"/i,
-//                 `<meta property="og:title" content="${res?.title}"`
-//               );
-//               data = data.replace(
-//                 /<meta property="og:url" content=".*?"/i,
-//                 `<meta property="og:url" content="/product/${res?.id}"`
-//               );
-//               data = data.replace(
-//                 /<meta property="og:description" content=".*?"/i,
-//                 `<meta property="og:description" content="${res.description}"`
-//               );
+              data = data.replace(
+                /<meta property="og:title" content=".*?"/i,
+                `<meta property="og:title" content="${res?.title}"`
+              );
+              data = data.replace(
+                /<meta property="og:url" content=".*?"/i,
+                `<meta property="og:url" content="/product/${res?.id}"`
+              );
+              data = data.replace(
+                /<meta property="og:description" content=".*?"/i,
+                `<meta property="og:description" content="${res.description}"`
+              );
               
-//               data = data.replace(
-//                 /<meta property="twitter:title" content=".*?"/i,
-//                 `<meta property="twitter:title" content="${res?.title}"`
-//               );
-//               data = data.replace(
-//                 /<meta property="twitter:url" content=".*?"/i,
-//                 `<meta property="twitter:url" content="/product/${res?.id}"`
-//               );
-//               data = data.replace(
-//                 /<meta property="twitter:description" content=".*?"/i,
-//                 `<meta property="twitter:description" content="${res.description}"`
-//               );
+              data = data.replace(
+                /<meta property="twitter:title" content=".*?"/i,
+                `<meta property="twitter:title" content="${res?.title}"`
+              );
+              data = data.replace(
+                /<meta property="twitter:url" content=".*?"/i,
+                `<meta property="twitter:url" content="/product/${res?.id}"`
+              );
+              data = data.replace(
+                /<meta property="twitter:description" content=".*?"/i,
+                `<meta property="twitter:description" content="${res.description}"`
+              );
               
-//               data = data.replace(
-//                 /<title>.*?<\/title>/i,
-//                 `<title>${res?.title}</title>`
-//               );
-//               data = data.replace(
-//                 /<link rel="canonical" href=".*?"/i,
-//                 `<link rel="canonical" href="/product/${res?.id}"`
-//               );
-//               data = data.replace(
-//                 /<meta name="description" content=".*?"/i,
-//                 `<meta name="description" content="${res?.description}" />`
-//               );
+              data = data.replace(
+                /<title>.*?<\/title>/i,
+                `<title>${res?.title}</title>`
+              );
+              data = data.replace(
+                /<link rel="canonical" href=".*?"/i,
+                `<link rel="canonical" href="/product/${res?.id}"`
+              );
+              data = data.replace(
+                /<meta name="description" content=".*?"/i,
+                `<meta name="description" content="${res?.description}" />`
+              );
               
           
           
           
 
-//       }
-//       console.log(data,"data")
+      }
+      console.log(data,"data")
 
-//       // Serve the modified HTML file content
-//       res.setHeader("Content-Type", "text/html");
-//       res.send(data);
-//     }
-//   });
-// });
+      // Serve the modified HTML file content
+      res.setHeader("Content-Type", "text/html");
+      res.send(data);
+    }
+  });
+});
 app.listen(5000, () => {
   console.log(`Example app listening on port ${5000}`);
 });
