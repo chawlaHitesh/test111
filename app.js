cd5314fe6a1bc625b3a27d793d71b25acb6c02c6
hiteshSoftwareEngineer786 <chawlahitesh8907@gmail.com>
2024-03-16 15:08:07 +0530

    product detail

--- app.js
+++ app.js
@@ -5,29 +5,88 @@ const fs = require("fs");
 console.log(path.join(__dirname, "/dist"));
 app.use(express.static(path.join(__dirname, "/dist")));
 
-const modifyHTMLContent = (htmlContent) => {
+const modifyHTMLContent = (data,{img,title,url,description}) => {
   // Modify the HTML content to include the desired header
-  htmlContent = htmlContent.replace(
-    /<head>/,
-    `<head>
-        <title>Your Page Title</title>
-        <meta name="description" content="Description of your page for search engines">
-        <!-- Open Graph / Facebook -->
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://yourwebsite.com">
-        <meta property="og:title" content="Your Page Title">
-        <meta property="og:description" content="Description of your page for Facebook">
-        <meta property="og:image" content="https://yourwebsite.com/your-image.jpg">
-        <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://yourwebsite.com">
-        <meta property="twitter:title" content="Your Page Title">
-        <meta property="twitter:description" content="Description of your page for Twitter">
-        <meta property="twitter:image" content="https://yourwebsite.com/your-image.jpg">
-        </head>`
+    data = data.replace(
+    /<meta property="og:image" content=".*?"/i,
+    `<meta property="og:image" content="${img}"`
   );
-  return htmlContent;
+  data = data.replace(
+    /<meta property="twitter:image" content=".*?"/i,
+    `<meta property="twitter:image" content="${img}"`
+  );
+  
+  data = data.replace(
+    /<meta property="og:title" content=".*?"/i,
+    `<meta property="og:title" content="${title}"`
+  );
+  data = data.replace(
+    /<meta property="og:url" content=".*?"/i,
+    `<meta property="og:url" content="${url}"`
+  );
+  data = data.replace(
+    /<meta property="og:description" content=".*?"/i,
+    `<meta property="og:description" content="${description}"`
+  );
+  
+  data = data.replace(
+    /<meta property="twitter:title" content=".*?"/i,
+    `<meta property="twitter:title" content="${title}"`
+  );
+  data = data.replace(
+    /<meta property="twitter:url" content=".*?"/i,
+    `<meta property="twitter:url" content="${url}"`
+  );
+  data = data.replace(
+    /<meta property="twitter:description" content=".*?"/i,
+    `<meta property="twitter:description" content="${description}"`
+  );
+  
+  data = data.replace(
+    /<title>.*?<\/title>/i,
+    `<title>${title}</title>`
+  );
+  data = data.replace(
+    /<link rel="canonical" href=".*?"/i,
+    `<link rel="canonical" href="${url}"`
+  );
+  data = data.replace(
+    /<meta name="description" content=".*?"/i,
+    `<meta name="description" content="${description}" />`
+  );
+  return data;
 };
+app.get("/product/:id",async(req,res)=>{
+    console.log(req.params?.id)
+    console.log("Requested path:", req.path);
+    const filePath = path.join(__dirname, "/dist", "index.html");
+    console.log(filePath, "filePath");
+    fs.readFile(filePath, "utf8", async (err, data) => {
+        let updatedData;
+      if (err) {
+        // Handle file not found or read error
+        console.error(err);
+        res.status(404).send("File not found");
+      } else {
+        if (req.path.startsWith("/product/")) {
+         
+          const resData = await fetch(
+            `https://dummyjson.com/products/${req.params?.id}`
+          );
+          const res = await resData.json();
+              console.log({data})
+              updatedData= modifyHTMLContent(data,{img:res?.thumbnail,description:res.description,title:res?.title,url:`/product/${res?.id}`}) 
+  
+        }
+        console.log(data,"data")
+  
+        // Serve the modified HTML file content
+        res.setHeader("Content-Type", "text/html");
+        res.send(updatedData);
+      }
+    })
+    // res.send("teting")
+})
 app.get("*", (req, res) => {
   console.log("Requested path:", req.path);
   const filePath = path.join(__dirname, "/dist", "index.html");
